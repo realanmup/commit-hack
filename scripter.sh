4e7ef466064b134de19c29a8620eeb0754daae07
Saurav M2 <realanmup@gmail.com>
2017-01-01 14:00:00 +0545

    updates for 2017-01-01

--- scripter.sh
+++ scripter.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 # Start date
-start_date="2023-04-06"
+start_date="2017-01-01"
 current_date=$start_date
+last_date=$start_date
 
 # Loop for 365 days
-for (( i=1; i<=365; i++ )); do
+for (( i=1; i<=2920; i++ )); do
     # Create a file with the current date as the filename
-    touch "${current_date}.txt"
+    mv "$last_date.txt" "$current_date.txt" || touch "$last_date.txt";
     git add . 
     git commit -m "updates for $current_date"
     
@@ -16,6 +17,7 @@ for (( i=1; i<=365; i++ )); do
     git commit --amend --no-edit --date="$current_date 14:00:00"
 
     # Move to the next day
+    last_date=current_date
     current_date=$(date -j -v +1d -f "%Y-%m-%d" "$current_date" "+%Y-%m-%d")
 done
 
